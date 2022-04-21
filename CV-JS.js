58205906fa6ea29b679c4dbcd7d5ac9ed732278e
MFarazAnjum <farazanjum.313@gmail.com>
2022-04-22 01:50:35 +0500

    CV-JS-Code

---
+++ CV-JS.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+    var bn = document.getElementById("bab");
+    var bn1 = document.getElementById("bab1");
+    var bn2 = document.getElementById("bab2");
+    var bn3 = document.getElementById("bab3");
+    var bn4 = document.getElementById("bab4");
+    var bn5 = document.getElementById("bab5");
+    var bn6 = document.getElementById("bab6");
+    var bn7 = document.getElementById("bab7");
+    bn.onclick = alt;
+    bn1.onclick = alt;
+    bn2.onclick = alt;
+    bn3.onclick = alt;
+    bn4.onclick = alt;
+    bn5.onclick = alt;
+    bn6.onclick = alt;
+    bn7.onclick = alt;
+}
+function alt(){
+    alert("Welcome to My CV");
+}
